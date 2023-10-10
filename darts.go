a0292650c1e57a849c02b37140d39dca0fd5eb51
Rustam Khasanshin <rustery@gmail.com>
2023-10-10 21:58:48 +0200

    improvements

--- darts.go
+++ darts.go
@@ -1,27 +1,35 @@
+// darts package is a game where the target rewards 4 different amounts of points,
+// depending on where the dart lands
 package darts
 
 import "math"
 
+// Circle is a struct that represents a circle of the darts target
 type Circle struct {
 	radius float64
 	points int
 }
 
-var availableCircles = []Circle{
-	{radius: 1, points: 10},
-	{radius: 5, points: 5},
-	{radius: 10, points: 1},
-}
+// Constants for the 3 circles of the darts target
+var BIG_CIRCLE = Circle{radius: 1, points: 10}
+var MIDDLE_CIRCLE = Circle{radius: 5, points: 5}
+var SMALL_CIRCLE = Circle{radius: 10, points: 1}
 
+// isInCircle checks if a point is inside a circle
 func isInCircle(x, y float64, c Circle) bool {
 	return math.Sqrt(x*x+y*y) <= c.radius
 }
 
+// Score calculates the score of a dart throw
 func Score(x, y float64) int {
-	for _, c := range availableCircles {
-		if isInCircle(x, y, c) {
-			return c.points
-		}
+	switch {
+	case isInCircle(x, y, BIG_CIRCLE):
+		return BIG_CIRCLE.points
+	case isInCircle(x, y, MIDDLE_CIRCLE):
+		return MIDDLE_CIRCLE.points
+	case isInCircle(x, y, SMALL_CIRCLE):
+		return SMALL_CIRCLE.points
+	default:
+		return 0
 	}
-	return 0
 }
